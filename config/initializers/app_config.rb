d4449516225ac6d67c1668e98dd67d2d713b6d1e
Cyril Rohr <cyril.rohr@gmail.com>
2015-12-08 22:05:04 +0000

    ga

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,3 +1,4 @@
 AppConfig = OpenStruct.new
 AppConfig.github_repo = ENV.fetch('GITHUB_REPO') { "crohr/ebarnouflant" }
 AppConfig.cache_reset_token = ENV.fetch('CACHE_RESET_TOKEN') { "secret" }
+AppConfig.google_analytics_id = ENV.fetch('GOOGLE_ANALYTICS_ID') { nil }
