8135ef6d7bdb68f4d3e836902682184fd90f383a
Cyril Rohr <cyril.rohr@gmail.com>
2015-12-08 21:49:04 +0000

    default value

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,3 +1,3 @@
 AppConfig = OpenStruct.new
-AppConfig.github_repo = ENV.fetch('GITHUB_REPO')
+AppConfig.github_repo = ENV.fetch('GITHUB_REPO') { "crohr/ebarnouflant" }
 AppConfig.cache_reset_token = ENV.fetch('CACHE_RESET_TOKEN') { "secret" }
